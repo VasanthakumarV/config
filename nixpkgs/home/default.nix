b048dbe311258f2c47069d1c203e14a650f5f87a
VasanthakumarV <vasanth260m12@gmail.com>
2021-12-21 00:14:19 +0530

    Add zellij and rust packages

--- nixpkgs/home/default.nix
+++ nixpkgs/home/default.nix
@@ -2,6 +2,7 @@
   imports = [
     (import ./programs.nix { inherit pkgs; })
     (import ./zsh.nix { inherit pkgs; })
+    (import ./rust.nix { inherit pkgs; })
     ./starship.nix
     ./alacritty.nix
     ./nvim
--- nixpkgs/home/programs.nix
+++ nixpkgs/home/programs.nix
@@ -1,3 +1,3 @@
 { pkgs, ... }: {
-  home.packages = with pkgs; [ exa bat ripgrep fd nixfmt bottom ];
+  home.packages = with pkgs; [ exa bat ripgrep fd bottom zellij nixfmt ];
 }
